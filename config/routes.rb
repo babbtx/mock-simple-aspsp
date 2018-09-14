eccd7de8d8a3d89a4d83839cc2989c6f22dd7ac1
David Babbitt <davidbabbitt@pingidentity.com>
2018-09-14 15:25:35 -0500

    make API v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   defaults format: 'json' do
-    scope '/OpenBanking/v1' do
+    scope '/OpenBanking/v2' do
       resources :accounts, only: [:index, :show]
     end
   end
