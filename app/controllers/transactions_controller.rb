71ac01ae830e30187ffaa2575ca70e2d9189d0ac
David Babbitt <davidbabbitt@pingidentity.com>
2018-09-19 10:23:13 -0500

    more clarification

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,7 @@ class TransactionsController < ApplicationController
     # start with the transactions owned by the user
     transactions = Transaction.for_user(current_user!)
 
-    # For controllers that are used either as top-level resource controllers or sub-resource controllers
+    # For controllers that are used both as top-level resource controllers or sub-resource controllers
     # this next line is "the right way" to do this in RoR: further restrict the data scope of "transactions
     # owned by the user" to include only the records "under" the parent resource, in this case the account.
     #
@@ -18,8 +18,9 @@ class TransactionsController < ApplicationController
     # More comments:
     #
     # 1) The token is theoretically validated at this point. The caller in question is a authenticated.
-    # An access control layer doesn't help prevent this data breach. For a similar real world bug
-    # leading to breach see https://krebsonsecurity.com/2018/08/fiserv-flaw-exposed-customer-data-at-hundreds-of-banks/
+    # An access control layer doesn't help prevent this data breach (not without looking at the URL component).
+    # For a similar real world bug leading to breach see:
+    # https://krebsonsecurity.com/2018/08/fiserv-flaw-exposed-customer-data-at-hundreds-of-banks/
     #
     # 2) A better RoR developer would use something like CanCanCan for resource-level authorization
     # within the code. It's sort of a way to do a double-check that the call below was done right.
