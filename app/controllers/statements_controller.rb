1910e46938bc4c6e5747c5f775eefc265bec1b42
David Babbitt <davidbabbitt@pingidentity.com>
2018-09-24 10:21:12 -0500

    add intentional bug

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -4,10 +4,14 @@ class StatementsController < ApplicationController
     statements = Statement.for_user(current_user!)
 
     # narrow down to the ones for the named account, if any
-    statements = statements.for_account(params[:account_id]) if params[:account_id].present?
+    # same intentional bug as described in TransactionsController#index
+    # statements = statements.for_account(params[:account_id]) if params[:account_id].present?
+    statements = Statement.for_account(params[:account_id]) if params[:account_id].present?
 
     statements = statements.oldest_first
     self_url = params[:account_id].present? ? account_statements_url(params[:account_id]) : statements_url
-    render json: StatementSerializer.new(statements, links: {Self: self_url}, meta: { TotalPages: 1 }).serializable_hash
+    render json: StatementSerializer.new(statements,
+                                         links: {Self: self_url},
+                                         meta: { TotalPages: 1 }).serializable_hash
   end
 end
