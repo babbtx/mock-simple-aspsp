4aa15d0332103723851b6ceb6204af9f69b1be85
David Babbitt <davidbabbitt@pingidentity.com>
2018-09-24 10:21:29 -0500

    get a single account statement, also with an intentional bug

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,4 +1,6 @@
 class StatementsController < ApplicationController
+  before_action :load_statement, only: :show
+
   def index
     # start with all statements for the user
     statements = Statement.for_user(current_user!)
@@ -13,5 +15,29 @@ class StatementsController < ApplicationController
     render json: StatementSerializer.new(statements,
                                          links: {Self: self_url},
                                          meta: { TotalPages: 1 }).serializable_hash
+  end
+
+  def show
+    render json: StatementSerializer.new(@statement,
+                                         links: {Self: account_statement_url(@statement.account_id, @statement.id)},
+                                         meta: { TotalPages: 1 }).serializable_hash
+  end
+
+  private
+
+  def load_statement
+    # Below is the "right" way to load a specifically named resource for an authenticated user:
+    # @statement = Statement.for_user(current_user!).for_account(params[:account_id]).find(params[:id])
+
+    # Above we search for statement by its identifier but only among the statements for this user and this account.
+    # Even if that statement exists for another user or account but is not among the statements for this user and
+    # account, find() will raise a RecordNotFound-type of error and in some superclass that gets mapped to 404.
+    # Therefore the API never discloses that a resource really exists if despite the user not being authorized to see it.
+
+    # Here is the wrong way to do it:
+    # First find the resource, which returns 404 if it doesn't exist.
+    @statement = Statement.find(params[:id])
+    # Next check that the user is authorized to see it, and return 403.
+    head(:forbidden) if @statement.account.owner != current_user!
   end
 end
