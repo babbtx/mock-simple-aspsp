0627b5f6ef61cc44bc2f255a468cc98e450a2ad9
David Babbitt <davidbabbitt@pingidentity.com>
2018-09-24 22:40:47 -0500

    eager load the other directories

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.eager_load_paths += Rails.root.join('lib')
+  config.eager_load_paths += Rails.root.join('app/serializers')
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
