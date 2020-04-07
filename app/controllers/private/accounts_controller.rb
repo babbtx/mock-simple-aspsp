b2a3f6929599f8c3c17b3110d16adf3a8e98bb6f
David Babbitt <dbabbitt@yahoo.com>
2020-04-07 01:44:23 -0500

    added test for private accounts API this is in JSON-API format

--- app/controllers/private/accounts_controller.rb
+++ app/controllers/private/accounts_controller.rb
@@ -4,18 +4,5 @@ module Private
       account = Account.find(params[:id])
       render json: PrivateAccountSerializer.new(account).serializable_hash
     end
-
-    private
-
-
-    def find_user
-      user = User.find_by(uuid: params[:user_id])
-      unless user
-        # generate data if we don't have one
-        user = User.create(uuid: params[:user_id])
-        AccountsGenerator.generate_accounts_for_user(user)
-      end
-      user
-    end
   end
 end
