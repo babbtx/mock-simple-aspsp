d22fb70062806fb25826a67ef91da2326f559051
David Babbitt <davidbabbitt@pingidentity.com>
2021-04-22 02:22:27 -0500

    support mock token

--- app/controllers/concerns/jwt_secured.rb
+++ app/controllers/concerns/jwt_secured.rb
@@ -8,22 +8,37 @@ module JwtSecured
 
   private
 
+  # raises JWT::VerificationError if an Authorization header JWT is not present or not valid
+  # by default, that exception renders :unauthorized
+  # sets @auth_payload with the decoded JWT or the mock token
   def authenticate_request!
     @auth_payload ||= begin
-      payload, header = JWT.decode(bearer_value, nil, false)
-      @auth_header = header.with_indifferent_access
-      payload.with_indifferent_access
+      val = nil
+      if mock_token.present?
+        val = JSON.parse(mock_token).with_indifferent_access
+      else
+        payload, header = JWT.decode(bearer_value, nil, false)
+        @auth_header = header.with_indifferent_access
+        val = payload.with_indifferent_access
+      end
+      logger.info "Bearer = #{val}"
+      val
     end
   end
 
   def bearer_value
     if request.headers['Authorization'].present?
-      request.headers['Authorization'].split(' ').last
+      request.headers['Authorization'].split(/\s+/).drop(1).join(' ')
     end
+  end
+
+  def mock_token
+    bearer_value if (bearer_value.presence || '').starts_with?('{')
   end
 
   def render_jwt_unauthorized
     logger.warn "JWT unauthorized. Bearer = #{bearer_value}"
     head :unauthorized
   end
+
 end
