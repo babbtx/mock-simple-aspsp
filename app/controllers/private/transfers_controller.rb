bf0d033fa604a52dfd35ff859736fcce726a7f5c
David Babbitt <davidbabbitt@pingidentity.com>
2021-09-14 01:17:06 -0500

    authorize the transfer with pingone passes account ids and transfer amount accepts denied-reason as a statement on error (with the payload being the string to return to the client)

--- app/controllers/private/transfers_controller.rb
+++ app/controllers/private/transfers_controller.rb
@@ -1,6 +1,9 @@
 module Private
   class TransfersController < ApiController
+    include ExternalAuthz
+
     rescue_from ActionController::ParameterMissing, with: :malformed_request
+    append_before_action :authorize_transfer
 
     # Transfer funds between any two accounts.
     def create
@@ -29,6 +32,22 @@ module Private
           status: Rack::Utils::HTTP_STATUS_CODES[400],
           title: ex.message
        }]}
+    end
+
+    def authorize_transfer
+      if ExternalAuthz.configured?
+        external_authorize!(transfer_params.as_json)
+      end
+    end
+
+    def render_authz_denied(ex)
+      reason = (ex.response_json['statements']||[]).collect{|s| s['payload'] if s['code'] == 'denied-reason'}.first
+      reason ||= 'Denied by authorization policy'
+      render status: 403, json: {errors: [{
+        code: 403,
+        status: Rack::Utils::HTTP_STATUS_CODES[403],
+        detail: reason
+      }]}
     end
   end
 end
